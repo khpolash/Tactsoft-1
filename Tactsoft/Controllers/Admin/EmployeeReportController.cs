3c8b0fe02a55309ccb374759dc27616f81feff63
Raju <rajuoffice220@gmail.com>
2023-04-18 09:50:50 +0600

    Controller and view for report

---
+++ Tactsoft/Controllers/Admin/EmployeeReportController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tactsoft.Service.Services;
+
+namespace Tactsoft.Controllers.Admin
+{
+    public class EmployeeReportController : Controller
+    {
+        private readonly IEmployeeReportService _employeeReportService;
+        public EmployeeReportController(IEmployeeReportService employeeReportService)
+        {
+            this._employeeReportService = employeeReportService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PrintEmpDetails(long id)
+        {
+            var data = _employeeReportService.GetEmployeeRepoprtData(id);
+            return View(data);
+        }
+        //public IActionResult DownloadInvoicePDF(long id)
+        //{
+        //    var result = _reportService.GetInvoiceReportData(id);
+        //    var rpt = new ViewAsPdf();
+        //    rpt.PageOrientation = Orientation.Portrait;
+        //    rpt.CustomSwitches = footer;
+        //    rpt.FileName = "Purchase_Invoice" + result.PurchaseViewModel.PurchaseCode + ".pdf";
+        //    rpt.ViewName = "DownloadInvoicePDF";
+        //    rpt.Model = result;
+        //    return rpt;
+        //}
+
+        //public ActionResult ReportByDateRange()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public ActionResult ReportByDateRange(DateTime startDate, DateTime endDate)
+        //{
+        //    ViewBag.StartDate = startDate.ToString("dd/MM/yyyy");
+        //    ViewBag.EndDate = endDate.ToString("dd/MM/yyyy");
+        //    var data = _reportService.ReportByDateRange(startDate, endDate);
+
+        //    return View(data);
+        //}
+
+
+    }
+}
